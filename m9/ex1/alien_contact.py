dd0c54fbb6c776aed167852305647ec7334a8108
Fetraniaina Paul Ralaiavy <fralaiav@c1r13s6.42antananarivo.mg>
2026-08-25 22:59:49 +0300

    correct

--- m9/ex1/alien_contact.py
+++ m9/ex1/alien_contact.py
@@ -23,9 +23,9 @@ class ContactType(str, Enum):
 
 class AlienContact(BaseModel):
     contact_id: str = Field(..., min_length=5, max_length=15)
-    timestamp: datetime
+    timestamp: datetime = Field()
     location: str = Field(..., min_length=3, max_length=100)
-    contact_type: ContactType
+    contact_type: ContactType = Field()
     signal_strength: float = Field(..., ge=0.0, le=10.0)
     duration_minutes: int = Field(..., ge=1, le=1440)
     witness_count: int = Field(..., ge=1, le=100)
--- m9/ex2/space_crew.py
+++ m9/ex2/space_crew.py
@@ -24,21 +24,21 @@ class Rank(str, Enum):
 class CrewMember(BaseModel):
     member_id: str = Field(..., min_length=3, max_length=10)
     name: str = Field(..., min_length=2, max_length=50)
-    rank: Rank
+    rank: Rank = Field()
     age: int = Field(..., ge=18, le=80)
     specialization: str = Field(..., min_length=3, max_length=30)
     years_experience: int = Field(..., ge=0, le=50)
-    is_active: bool = True
+    is_active: bool = Field(default=True)
 
 
 class SpaceMission(BaseModel):
     mission_id: str = Field(..., min_length=5, max_length=15)
     mission_name: str = Field(..., min_length=3, max_length=100)
     destination: str = Field(..., min_length=3, max_length=50)
-    launch_date: datetime
+    launch_date: datetime = Field()
     duration_days: int = Field(..., ge=1, le=3650)
     crew: list[CrewMember] = Field(..., min_length=1, max_length=12)
-    mission_status: str = "Plainned"
+    mission_status: str = "Planned"
     budget_millions: float = Field(..., ge=1.0, le=10000.0)
 
     @model_validator(mode="after")
